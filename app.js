ef70d6a9ae554d551d02dedacde6bf9b0d36c7af
zzeleznick <zzeleznick@gmail.com>
2014-09-21 10:23:55 -0700

    testing

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
-/*var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
-}); */
-
-require('http').createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
+var port = Number(process.env.PORT || 3000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
